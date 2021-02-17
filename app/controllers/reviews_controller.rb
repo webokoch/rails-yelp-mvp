d67394338325a72e240163ee5eeb54dac3574a1b
Stefan Koch <webokoch@gmx.de>
2021-02-17 18:27:28 +0100

    refactored ReviewsController & improved star rating

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,10 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:new, :create]
   def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     if @review.save
@@ -19,5 +18,9 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:rating, :content)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
