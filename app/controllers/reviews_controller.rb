4557000a480b1c5e8f4648e75275d80a416dceee
Stefan Koch <webokoch@gmx.de>
2021-02-17 16:11:14 +0100

    reviews can be displayed and entered

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,23 @@
 class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
 
-  # def new
-  #   @restaurant = Restaurant.find(params[:restaurant_id])
-  #   @review  = Review.new
-  # end
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'new'
+    end
+  end
 
-  # def create
+  private
 
-  # end
-
-  # private
-
-  # def review_params
-  #   params.require(:review).permit(:rating, :content)
-  # end
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
